ac1c048af7a4303fa577b7cea1d3b1337a83fd16
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-02-25 23:57:23 -0800

    daily

--- index.js
+++ index.js
@@ -47,14 +47,92 @@ Object.defineProperties(String.prototype, {
 define("example/browser", ["require", "exports", "Gtk", "WebKit"], function (require, exports, Gtk, WebKit) {
     "use strict";
     print("Hello!");
+    var argv = ARGV;
     Gtk.init(null);
-    var win = new Gtk.Window({
+    var window = new Gtk.Window({
         title: 'jsGtk+ browser',
         type: Gtk.WindowType.TOPLEVEL,
         window_position: Gtk.WindowPosition.CENTER
     });
     var webView = new WebKit.WebView();
-    win.connect('delete-event', Gtk.main_quit);
-    win.show_all();
-    Gtk.main();
+    var toolbar = new Gtk.Toolbar();
+    // buttons to go back, go forward, or refresh
+    var button = {
+        back: Gtk.ToolButton.new_from_stock(Gtk.STOCK_GO_BACK),
+        forward: Gtk.ToolButton.new_from_stock(Gtk.STOCK_GO_FORWARD),
+        refresh: Gtk.ToolButton.new_from_stock(Gtk.STOCK_REFRESH)
+    };
+    // where the URL is written and shown
+    var urlBar = new Gtk.Entry();
+    // the browser container, so that's scrollable
+    var scrollWindow = new Gtk.ScrolledWindow({});
+    // horizontal and vertical boxes
+    var hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+    var vbox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL });
+    // Setting up optional Dark theme (gotta love it!)
+    // ./browser.js google.com --dark
+    if (argv.some(function (info) { return info === '--dark'; })) {
+        var gtkSettings = Gtk.Settings.get_default();
+        gtkSettings.gtk_application_prefer_dark_theme = true;
+        gtkSettings.gtk_theme_name = 'Adwaita';
+    }
+    else if (argv.some(function (info) { return info === '--light'; })) {
+        var gtkSettings = Gtk.Settings.get_default();
+        gtkSettings.gtk_application_prefer_dark_theme = false;
+        gtkSettings.gtk_theme_name = 'Adwaita';
+    }
+    // open first argument or Google
+    webView.load_uri(url(argv.filter(function (url) { return '-' !== url[0]; })[0] || 'google.com'));
+    // whenever a new page is loaded ...
+    // webView.connect('load-changed', (widget, loadEvent, data) => {
+    //     switch (loadEvent) {
+    //       case 2: // XXX: where is WEBKIT_LOAD_COMMITTED ?
+    //         // ... update the URL bar with the current adress
+    //         urlBar.set_text(widget.get_uri());
+    //         button.back.set_sensitive(webView.can_go_back());
+    //         button.forward.set_sensitive(webView.can_go_forward());
+    //         break;
+    //     }
+    // });
+    // configure buttons actions
+    button.back.connect('clicked', function () { return webView.go_back(); });
+    button.forward.connect('clicked', function () { return webView.go_forward(); });
+    button.refresh.connect('clicked', function () { return webView.reload(); });
+    // enrich the toolbar
+    toolbar.add(button.back);
+    toolbar.add(button.forward);
+    toolbar.add(button.refresh);
+    // define "enter" / call-to-action event
+    // whenever the url changes on the bar
+    urlBar.connect('activate', function () {
+        var href = url(urlBar.get_text());
+        urlBar.set_text(href);
+        webView.load_uri(href);
+    });
+    // make the container scrollable
+    scrollWindow.add(webView);
+    // pack horizontally toolbar and url bar
+    hbox.pack_start(toolbar, false, false, 0);
+    hbox.pack_start(urlBar, true, true, 8);
+    // pack vertically top bar (hbox) and scrollable window
+    vbox.pack_start(hbox, false, true, 0);
+    vbox.pack_start(scrollWindow, true, true, 0);
+    // configure main window
+    window.set_default_size(1024, 720);
+    window.set_resizable(true);
+    window.connect('show', function () {
+        // bring it on top in OSX
+        window.set_keep_above(true);
+        Gtk.main();
+    });
+    window.connect('destroy', function () { return Gtk.main_quit(); });
+    window.connect('delete_event', function () { return false; });
+    // add vertical ui and show them all
+    window.add(vbox);
+    window.show_all();
+    // little helper
+    // if link doesn't have a protocol, prefixes it via http://
+    function url(href) {
+        return /^([a-z]{2,}):/.test(href) ? href : ('http://' + href);
+    }
 });
