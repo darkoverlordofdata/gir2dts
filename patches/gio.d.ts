d99942daa1f15d18c885b56abbf15b4fbc78f9a5
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-03-24 21:39:39 -0700

    add bin

--- patches/gio.d.ts
+++ patches/gio.d.ts
@@ -1,5 +1,12 @@
     export class File extends GObject.Object {
         query_exists(obj:any): boolean
         load_contents(obj:any): any
+        make_directory(obj:any): boolean
+        delete(obj:any): boolean
+        create(flags:any, obj: any): any
+        get_basename(): string
+        get_path(): string
+        get_parent():File
         static new_for_path(path: string): File
     }
+
